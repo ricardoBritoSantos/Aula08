5d61ac70a5cb36e205e781afb6ed889640ea2af7
Ricardo <ricardo_brito_santos@hotmail.com>
2021-06-19 15:22:42 -0300

    Exercicio 4 de Return feito

--- src/Desafio04.java
+++ src/Desafio04.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Desafio04 {
 	/* 
@@ -5,7 +6,28 @@ public class Desafio04 {
 	
 	Crie as funções que julgar necessárias para obter cada um dos valores que fazem parte do salário de um professor
 	 * */
-	public static void main(String[] args) {
+	
+	
+	public static double calcularSalario(int numeroAulas, double horaAula) {
 		
+		var salarioBase = numeroAulas * 4.5 * horaAula;
+		var horaAtividade = salarioBase * 0.05;
+		var descansoSemanalRemunerado = (salarioBase + horaAtividade) / 6;
+		var salario = salarioBase + horaAtividade + descansoSemanalRemunerado;
+		
+		return salario;
+	}
+	
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Para calcular seu salário base precisamos saber quantas aulas semanais o professor tem na instituição");
+		var numeroAulas = leitor.nextInt();
+		System.out.println("Para calcular seu salário base precisamos saber qual é o valor da hora aula praticada nessa instituição");
+		var horaAula = leitor.nextDouble();
+		
+		System.out.println("Salário total R$ " + calcularSalario(numeroAulas, horaAula));
+		
+		leitor.close();
 	}
 }
